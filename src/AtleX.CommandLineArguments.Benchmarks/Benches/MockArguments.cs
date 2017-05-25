f54d2adb4255959f3e1caca86b81fe696e3b1076
Alex Kamsteeg <alex@atlex.nl>
2017-05-25 20:35:17 +0200

    Added basic benchmarks with BenchmarkDotNet

---
+++ src/AtleX.CommandLineArguments.Benchmarks/Benches/MockArguments.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AtleX.CommandLineArguments.Benchmarks.Benches
+{
+  public class MockArguments
+    : Arguments
+  {
+    [Required]
+    public bool Toggle
+    {
+      get;
+      set;
+    }
+
+    public string String
+    {
+      get;
+      set;
+    }
+
+    public ArgumentValues Enum
+    {
+      get;
+      set;
+    }
+  }
+
+  public enum ArgumentValues
+  {
+    True,
+    False,
+    Maybe
+  }
+}
