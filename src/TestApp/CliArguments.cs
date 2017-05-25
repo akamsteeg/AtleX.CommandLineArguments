5c243bc1e1624f73285f7ea25530c0d4d0750105
Alex Kamsteeg <alex@atlex.nl>
2017-05-25 15:42:56 +0200

    Added a basic test application

---
+++ src/TestApp/CliArguments.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AtleX.CommandLineArguments;
+
+namespace TestApp
+{
+  internal sealed class CliArguments
+    : Arguments
+  {
+    [Required(ErrorMessage = "ShowValidationResult is required")]
+    public bool ShowValidationResult
+    {
+      get;
+      set;
+    }
+  }
+}
---
+++ src/TestApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using AtleX.CommandLineArguments;
+using AtleX.CommandLineArguments.Validators;
+
+namespace TestApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      if (!CommandLineArguments.TryParse(args, out CliArguments cliArguments, out IEnumerable<ValidationResult> validationResults))
+      {
+        Console.WriteLine("Validation results;");
+
+        foreach (var currentValidationResults in validationResults)
+        {
+          Console.WriteLine($"* {currentValidationResults.ArgumentName}: {currentValidationResults.ValidationMessage} ({currentValidationResults.IsValid})");
+        }
+      }
+
+      Console.ReadKey();
+    }
+  }
+}
