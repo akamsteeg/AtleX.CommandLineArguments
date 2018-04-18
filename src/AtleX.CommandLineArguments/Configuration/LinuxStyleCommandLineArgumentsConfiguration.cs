7f3d92d9e8056df04362efef6aff82529ef9f90e
Alex Kamsteeg <alex@atlex.nl>
2018-04-18 18:56:03 +0200

    Added the LinuxStyleCommandLineArgumentsConfiguration to simplify choosing for linux-style arguments

---
+++ src/AtleX.CommandLineArguments/Configuration/LinuxStyleCommandLineArgumentsConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using AtleX.CommandLineArguments.Help;
+using AtleX.CommandLineArguments.Parsers;
+
+
+namespace AtleX.CommandLineArguments.Configuration
+{
+  /// <summary>
+  /// Represents the <see cref="CommandLineArgumentsConfiguration"/> for
+  /// Linux-style (--key value) command line arguments
+  /// </summary>
+  public class LinuxStyleCommandLineArgumentsConfiguration
+    : CommandLineArgumentsConfiguration
+  {
+    /// <summary>
+    /// Initializes a new instance of <see cref="LinuxStyleCommandLineArgumentsConfiguration"/>
+    /// </summary>
+    public LinuxStyleCommandLineArgumentsConfiguration()
+    {
+      this.Parser = new LinuxStyleCommandLineArgumentsParser();
+      this.HelpWriter = new LinuxStyleHelpWriter();
+    }
+  }
+}
