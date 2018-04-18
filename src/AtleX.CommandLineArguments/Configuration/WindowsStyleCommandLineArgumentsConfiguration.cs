6a5415ba0dd533a5142aa402fcf42aadc6cf1080
Alex Kamsteeg <alex@atlex.nl>
2018-04-18 19:17:10 +0200

    Re-added the WindowsStyleCommandLineArgumentsConfiguration after an accidental revert

---
+++ src/AtleX.CommandLineArguments/Configuration/WindowsStyleCommandLineArgumentsConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using AtleX.CommandLineArguments.Help;
+using AtleX.CommandLineArguments.Parsers;
+
+namespace AtleX.CommandLineArguments.Configuration
+{
+  /// <summary>
+  /// Represents the <see cref="WindowsStyleCommandLineArgumentsConfiguration"/> for
+  /// Windows-style (/key value) command line arguments
+  /// </summary>
+  public class WindowsStyleCommandLineArgumentsConfiguration
+    : CommandLineArgumentsConfiguration
+  {
+    /// <summary>
+    /// Initializes a new instance of <see cref="WindowsStyleCommandLineArgumentsConfiguration"/>
+    /// </summary>
+    public WindowsStyleCommandLineArgumentsConfiguration()
+    {
+      this.Parser = new WindowsStyleCommandLineArgumentsParser();
+      this.HelpWriter = new WindowsStyleHelpWriter();
+    }
+  }
+}
