77e301ef4b1b7b3c2cd0b6eacb32c71b677b8570
Alex Kamsteeg <alex@atlex.nl>
2018-05-08 22:14:00 +0200

    Added unit tests for ParseResult and ValidationError

---
+++ src/AtleX.CommandLineArguments.Tests/Parsers/ParseResultTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using AtleX.CommandLineArguments.Parsers;
+using AtleX.CommandLineArguments.Tests.Mocks;
+using AtleX.CommandLineArguments.Validators;
+using NUnit.Framework;
+
+namespace AtleX.CommandLineArguments.Tests.Parsers
+{
+  [TestFixture]
+  public class ParseResultTests
+  {
+    [Test]
+    public void Ctor_WithNullCommandLineArguments_Throws()
+    {
+      Assert.Throws<ArgumentNullException>(() => 
+        new ParseResult<TestArguments>(null, Enumerable.Empty<ValidationError>()));
+    }
+
+    [Test]
+    public void Ctor_WithNullValidationErrors_Throws()
+    {
+      Assert.Throws<ArgumentNullException>(() =>
+        new ParseResult<TestArguments>(new TestArguments(), null));
+    }
+  }
+}
---
+++ src/AtleX.CommandLineArguments.Tests/Validators/ValidationErrorTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using AtleX.CommandLineArguments.Validators;
+using NUnit.Framework;
+
+namespace AtleX.CommandLineArguments.Tests.Validators
+{
+  [TestFixture]
+  public class ValidationErrorTests
+  {
+    [Test]
+    public void Ctor_WithNullArgumentName_Throws()
+    {
+      Assert.Throws<ArgumentNullException>(() =>
+        new ValidationError(null, "validatorName", ""));
+    }
+
+    [Test]
+    public void Ctor_WithEmptyArgumentName_Throws()
+    {
+      Assert.Throws<ArgumentNullException>(() =>
+        new ValidationError("", "validatorName", ""));
+    }
+
+    [Test]
+    public void Ctor_WithNullValidatorName_Throws()
+    {
+      Assert.Throws<ArgumentNullException>(() =>
+        new ValidationError("argumentName", null, ""));
+    }
+
+    [Test]
+    public void Ctor_WithEmptyValidatorName_Throws()
+    {
+      Assert.Throws<ArgumentNullException>(() =>
+        new ValidationError("argumentName", "", ""));
+    }
+
+    [Test]
+    public void Ctor_WithEmptyValidatorErrorMessage_DoesNotThrow()
+    {
+      Assert.DoesNotThrow(() =>
+        new ValidationError("argumentName", "validatorName", ""));
+    }
+  }
+}
